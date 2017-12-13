4a80e48b6c7f042ac0ad4a2c58bca8ecde493d8a
David Panzoli (PC-GAME) <david.panzoli@unv-jfc.fr>
2017-12-13 10:33:35 +0100

    Premier import du projet template Unity

---
+++ TempateProjectGAME/Assets/SteamVR/Prefabs/GlobalController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+		string[] devices = UnityEngine.Input.GetJoystickNames ();
+		for(int i = 0; i < devices.Length ; i++)
+		{
+			Debug.Log (devices [i]);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ TempateProjectGAME/Assets/SteamVR/Prefabs/ViveControllerInputTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViveControllerInputTest : MonoBehaviour {
+
+	private SteamVR_TrackedObject trackedObj;
+
+	private SteamVR_Controller.Device Controller
+	{
+		get { return SteamVR_Controller.Input((int)trackedObj.index); }
+	}
+
+	void Awake()
+	{
+		trackedObj = GetComponent<SteamVR_TrackedObject>();
+
+		Debug.Log ("ici");
+		GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube.transform.position = new Vector3(0, 0, 1);
+		//cube.transform.parent = trackedObj.GetComponent<Transform>();
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+	
+		if (Controller.GetHairTriggerDown())
+		{
+			Debug.Log ("trigger down");
+			this.transform.Find("Cube").transform.localScale.Set(0.3f,0.3f,0.3f);
+		}
+
+		// 3
+		if (Controller.GetHairTriggerUp())
+		{
+			this.transform.Find ("Cube").transform.localScale.Set (0.2f, 0.2f, 0.2f);
+		}
+
+	}
+
+
+
+}
